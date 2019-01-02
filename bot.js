4ceb611651077d215d35b2d591b8bb14e254ed66
KStudios-zz <kronco.studios@gmail.com>
2019-01-02 00:13:16 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const {Attachment } = require('discord.js');
 const client = new Discord.Client();
+var token = process.env.TOKEN;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -33,4 +34,4 @@ client.on('message', msg => {
 }
 );
 
-client.login(os.environ["TOKEN"]);
+client.login(token);
